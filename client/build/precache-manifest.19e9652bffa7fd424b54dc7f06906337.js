cee244f397cfb48fb22ea8c3ac0ae347e64c0ba7
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-11-15 00:47:18 +0000

    update gitignore

---
+++ client/build/precache-manifest.19e9652bffa7fd424b54dc7f06906337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4b4bc3c4ce6996e6e60a6e26069d9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d1074ee9a2b7302ef3",
+    "url": "/static/css/main.574e76db.chunk.css"
+  },
+  {
+    "revision": "5778204552679c099ee0",
+    "url": "/static/js/2.dce1f92b.chunk.js"
+  },
+  {
+    "revision": "07d1074ee9a2b7302ef3",
+    "url": "/static/js/main.051bbfc5.chunk.js"
+  },
+  {
+    "revision": "740a109da34f089eea60",
+    "url": "/static/js/runtime-main.99760884.js"
+  }
+]);
