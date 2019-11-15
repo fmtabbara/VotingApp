3266171d03aa760ec9a29346994cbb978b4f3e36
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2019-11-15 23:20:54 +0000

    Build

---
+++ client/build/precache-manifest.9474edcac2b150a1739e925fa71a5dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b795a7c6132ee5085a830466b0dc72d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00df38fd7064c388bf0a",
+    "url": "/static/css/main.9ad92592.chunk.css"
+  },
+  {
+    "revision": "f87912966e698c4b78ba",
+    "url": "/static/js/2.99b1fa20.chunk.js"
+  },
+  {
+    "revision": "00df38fd7064c388bf0a",
+    "url": "/static/js/main.1e0bac5f.chunk.js"
+  },
+  {
+    "revision": "740a109da34f089eea60",
+    "url": "/static/js/runtime-main.99760884.js"
+  }
+]);
